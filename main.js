a3eb3ab60cb3bae7b1ae5449388fb4900f74814a
Sathish M <sathishms0305@gmail.com>
2024-01-13 22:59:27 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let container = document.getElementById('container')
+let togglebtn = document.getElementById('toggleBtn')
+
+togglebtn.innerHTML = `<i class="fa-solid fa-circle-half-stroke fa-lg" style="color: #ffffff;"></i>`
+let toggler = ()=>{
+    let a = container.classList.toggle('light-container')
+    togglebtn.classList.toggle('tbutton-light')
+    if (a === true){
+        togglebtn.innerHTML = `<i class="fa-solid fa-circle-half-stroke fa-flip-horizontal fa-lg" style="color: #ffffff;"></i>`
+    }else{
+        togglebtn.innerHTML = `<i class="fa-solid fa-circle-half-stroke fa-lg" style="color: #ffffff;"></i>`
+    }
+}
+
+togglebtn.addEventListener('click',toggler)
