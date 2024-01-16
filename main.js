832c69c494906a0048e2a47b1e7d449aedbd934e
Sathish M <sathishms0305@gmail.com>
2024-01-16 10:59:44 +0530

    functionalities added

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let container = document.getElementById('container')
 let togglebtn = document.getElementById('toggleBtn')
 let equation = document.getElementById('equation')
 let display = document.getElementById('display')
-console.log(equation)
+console.log(equation.innerHTML)
 console.log(display)
 
 togglebtn.innerHTML = `<i class="fa-solid fa-circle-half-stroke fa-lg" style="color: #ffffff;"></i>`
@@ -11,8 +11,16 @@ let toggler = ()=>{
     togglebtn.classList.toggle('tbutton-light')
     if (a===true){
         togglebtn.innerHTML = `<i class="fa-solid fa-circle-half-stroke fa-flip-horizontal fa-lg" style="color: #ffffff;"></i>`
+        equation.classList.remove('dark-equation')
+        display.classList.remove('dark-display')
+        equation.classList.add('light-equation')
+        display.classList.add('light-display')
     }else{
         togglebtn.innerHTML = `<i class="fa-solid fa-circle-half-stroke fa-lg" style="color: #ffffff;"></i>`
+        equation.classList.add('dark-equation')
+        display.classList.add('dark-display')
+        equation.classList.remove('light-equation')
+        display.classList.remove('light-display')
     }
 }
 togglebtn.addEventListener('click',toggler)
